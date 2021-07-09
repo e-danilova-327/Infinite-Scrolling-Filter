c2cc77706e500bfea492798f13928daf9154d0c1
Elena <e.cotar327@gmail.com>
2021-07-09 17:40:15 +0300

    Changing divs in html, the fetching stuff in js and adjusting some css

--- main.js
+++ main.js
@@ -11,30 +11,39 @@ const filtering = () => {
 const filter = document.getElementById('filter');
 filter.addEventListener('input', filtering);
 
+let postsLimit = 3;
+let pageNr = 1;
+
 const postListing = () => {
-    fetch(postsUrl)
+    let postsLimit = 3;
+    let pageNr = 1;
+    const urlToFetch = `${postsUrl}?_limit=${postsLimit}&_page=${pageNr}`;
+    fetch(urlToFetch)
         .then((response) => response.json())
         .then((response) => {
             console.log(response);
-            for (let i = 0; i < 3; i++) {
-                /*title.innerText = `${response[i].title}`;
-                body.innerText = `${response[i].body}`;*/
+            response.forEach((response) => {
+                const postsDiv = document.getElementById('forPosts');
                 const e = document.createElement('div');
                 e.classList.add('div-class');
-                e.innerHTML = `<h2>${response[i].title}</h2> <p>${response[i].body}</p>`;
-                document.body.appendChild(e);
-                const load = document.getElementById('load');
-                document.body.appendChild(load);
-            }
+                e.innerHTML = `<div class="numerotation">${response.id}</div> <h2>${response.title}</h2> <p>${response.body}</p>`;
+                postsDiv.appendChild(e);
+            });
         });
+};
+
+const loader = document.getElementById('loader');
+
+const showLoader = async () => {
+    loader.classList.add('show');
+    pageNr++;
+    await postListing();
+    loader.classList.remove('show');
 };
 
 postListing();
 
-filtering();
-
 document.addEventListener('scroll', () => {
     postListing();
-    const load = document.getElementById('load');
-    document.body.removeChild(load);
+    showLoader();
 });
